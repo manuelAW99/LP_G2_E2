abb4fe061c5416b6fbec4b2461977d29d744e025
Jaime_Perez <jaime43lenin@gmail.com>
2021-10-09 01:20:37 -0400

    basic compiled succesfully

--- linked_list.cpp
+++ linked_list.cpp
@@ -5,24 +5,27 @@ using namespace std;
 template<typename T>
 class Node 
 {
-  T data;
-  std::shared_ptr<Node<T>> previous;
-  std::shared_ptr<Node<T>> next;
-  Node(){};
-  Node(T data): data(data){};
+  public:
+    T data;
+    shared_ptr< Node<T> > previous;
+    shared_ptr< Node<T> > next;
+    Node(){};
+    Node(T data): data(data){};
 };
 template<typename T>
 class Double_Linked_List
 {
   private:
-    std::shared_ptr<Node<T>> head, tail;
+    shared_ptr< Node<T> > head, tail;
     int size;
   public:
     Double_Linked_List(): size(0){};
 
     void Add(T data)
     {
-      Node<T> temp = std::make_shared<Node<T>>(data);
+      shared_ptr< Node<T> > temp (new Node<T>(data));
+      
+      
       if (size == 0)
       {
         head = temp;
@@ -39,44 +42,51 @@ class Double_Linked_List
       } 
       
     };
-    void* operator [](int index)
+    T operator [](int index)
     {
       if (index >= size || index < size) throw "Index out of range";
-      std::shared_ptr<Node<T>> temp = std::make_shared<Node<T>>();
+      shared_ptr< Node<T> > temp;
       temp = head;
       int current = 0;
 
       while(index != current)
       {
-        temp = temp->next;
+        temp = temp.next;
         current++;
       }
-      return temp->data;
+      return temp.data;
     };
     void Remove (T removing)
     {
-      std::shared_ptr<Node<T>> temp = std::make_shared<Node<T>>();
-      temp->next = head;
+      shared_ptr< Node<T> > temp;
+      temp.next = head;
       
-      while (temp->data != &removing)
-        temp = temp->next;
+      while (temp.data != removing)
+        temp = temp.next;
       
-      temp->previous->next = temp->next;
-      temp->next->previous = temp->next;
+      temp.previous.next = temp.next;
+      temp.next.previous = temp.next;
     }
     void Remove_At (int index)
     {
       if (index >= size || index < size) throw "Index out of range";
-      std::shared_ptr<Node<T>> temp = std::make_shared<Node<T>>();
+      shared_ptr< Node<T> > temp;
       temp = head;
       int current = 0;
 
       while(index != current)
       {
-        temp = temp->next;
+        temp = temp.next;
         current++;
       }
-      temp->previous->next = temp->next;
-      temp->next->previous = temp->next;
+      temp.previous.next = temp.next;
+      temp.next.previous = temp.next;
     }
 };
+
+int main()
+{
+  Double_Linked_List<int> temp;
+  temp.Add(2);
+  
+}
