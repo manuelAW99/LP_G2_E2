5aeb20c620cd97822ecb2f88c1491c94773cee30
Jaime_Perez <jaime43lenin@gmail.com>
2021-10-09 14:55:22 -0400

    almost end

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 #include<memory>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
-using namespace std;
+template<typename T>
+using sptr = std::shared_ptr<T>;
 template<typename T>
 class Node 
 {
   public:
     T data;
-    shared_ptr< Node<T> > previous;
-    shared_ptr< Node<T> > next;
+    sptr < Node<T> > previous;
+    sptr < Node<T> > next;
     Node(){};
     Node(T data): data(data){};
 };
@@ -16,14 +20,83 @@ template<typename T>
 class Double_Linked_List
 {
   public:
-    shared_ptr< Node<T> > head, tail;
+    sptr < Node<T> > head, tail;
     int size;
   public:
     Double_Linked_List(): size(0){};
 
+    Double_Linked_List(T data)
+    {
+      Add(data);
+    }
+
+    //Copy
+    Double_Linked_List(const Double_Linked_List& list)
+    {
+      auto temp = list.head;
+      while(temp != nullptr)
+      {
+        Add(temp.data);
+        temp = temp.next;
+      }
+    }
+
+    //Movement
+    Double_Linked_List(Double_Linked_List&& list)
+    {
+      *this = move(list);
+    }
+
+    //By copy
+    Double_Linked_List& operator= (Double_Linked_List& list)
+    {
+      head = nullptr;
+      tail = nullptr;
+      size = 0;
+
+      auto temp = list.head;
+      while(temp != nullptr)
+      {
+        Add(temp.data);
+        temp = temp.next;
+      }
+
+    }
+    //by Movement
+    Double_Linked_List& operator= (Double_Linked_List&& list)
+    {
+      head = nullptr;
+      tail = nullptr;
+      size = 0;
+
+      swap(head, list.head);
+      swap(tail, list.tail);
+      swap(size, list.size);  
+
+    }
+
+    Double_Linked_List(std::initializer_list<T> list)
+    {
+      for_each(list.begin(), list.end(), [this](T n) {Add(n);});
+    }
+
+    T operator [](int index)
+    {
+      if (index >= size || index < size) throw "Index out of range";
+      auto temp = head;
+      int current = 0;
+
+      while(index != current)
+      {
+        temp = temp.next;
+        current++;
+      }
+      return temp.data;
+    };
+
     void Add(T data)
     {
-      shared_ptr< Node<T> > temp (new Node<T>(data));
+      sptr < Node<T> > temp (new Node<T>(data));
       
       
       if (size == 0)
@@ -42,23 +115,10 @@ class Double_Linked_List
       } 
       
     };
-    T operator [](int index)
-    {
-      if (index >= size || index < size) throw "Index out of range";
-      shared_ptr< Node<T> > temp;
-      temp = head;
-      int current = 0;
-
-      while(index != current)
-      {
-        temp = temp.next;
-        current++;
-      }
-      return temp.data;
-    };
+    
     void Remove (T removing)
     {
-      shared_ptr< Node<T> > temp;
+      sptr < Node<T> > temp;
       temp.next = head;
       
       while (temp.data != removing)
@@ -70,7 +130,7 @@ class Double_Linked_List
     void Remove_At (int index)
     {
       if (index >= size || index < size) throw "Index out of range";
-      shared_ptr< Node<T> > temp;
+      sptr < Node<T> > temp;
       temp = head;
       int current = 0;
 
@@ -82,12 +142,17 @@ class Double_Linked_List
       temp.previous.next = temp.next;
       temp.next.previous = temp.next;
     }
+    ~Double_Linked_List()
+    {
+      head = nullptr;
+      tail = nullptr;
+      size = 0;
+    }
 };
 
 int main()
 {
   Double_Linked_List<int> temp;
   temp.Add(2);
-  int a = temp.head->data;
-  cout << a << endl;
+  
 }
